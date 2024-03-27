5939ea4cd90c102941eb28a3bd6b9a63e4f3460b
rioniii <148160573+rioniii@users.noreply.github.com>
2024-03-27 13:30:12 +0100

    TEST

--- reactapp1.client/src/main.jsx
+++ reactapp1.client/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-//TEST 
-//TESTReactDOM.createRoot(document.getElementById('root')).render(
-ReactDOM.createRoot(document.getElementById('root')).render(
+React 
+
+fedkfeh
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
