a775e45b6f34eb40a6c75ccd55be4ab094c7077e
meriton <137052060+meriton41@users.noreply.github.com>
2024-04-03 14:40:58 +0200

    Slider

--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -1,17 +1,23 @@
+import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import Card from "./Card";
-
+import IntervalSlider from './IntervalSlider';
+import image1 from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.jpg';
+import image4 from './assets/image4.jpg';
+import docPic from './assets/docPic.jpg';
 function App() {
-
+    const images = [image1, image2, image3, image4, docPic];
     return (
-           <>   
-                <Header></Header>
-                <Card></Card>
-                <Footer></Footer>
-            </>
-        
-
+        <div>
+            <Header />
+            <IntervalSlider images={images} interval={3000} />
+            <Card />
+            <Footer />
+        </div>
     );
 }
+
 export default App;
---
+++ reactapp1.client/src/IntervalSlider.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const IntervalSlider = ({ images, interval = 3000 }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const sliderInterval = setInterval(() => {
+            setCurrentIndex((prevIndex) =>
+                prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            );
+        }, interval);
+
+        return () => clearInterval(sliderInterval);
+    }, [images.length, interval]);
+
+    return (
+        <div>
+            <img src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} />
+        </div>
+    );
+};
+
+export default IntervalSlider;
