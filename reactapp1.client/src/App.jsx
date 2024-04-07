03ccac9be137790b5c53448d983dd27194823477
rioniii <148160573+rioniii@users.noreply.github.com>
2024-04-07 23:43:33 +0200

    Modifikimi

--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./Footer";
 import Card from "./Card";
 import IntervalSlider from './IntervalSlider';
-import i2 from './assets/i2.jpg';
-import i3 from './assets/i3.jpg';
-import i4 from './assets/i4.jpg'; import Container from 'react-bootstrap/Container';
+import Ballina from './Ballina';
+
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import i4 from './assets/i4.jpg';
+import i2 from './assets/i2.jpg';
+import i3 from './assets/i3.jpg';
+
 import { Route, NavLink, Routes, HashRouter } from 'react-router-dom';
+
 const RegisterForm = React.lazy(() => import("./RegisterForm"));
   const Header = () => {
       const images = [i3, i2, i4];
@@ -19,27 +27,29 @@ const RegisterForm = React.lazy(() => import("./RegisterForm"));
               <header>
                   <Navbar bg="dark" data-bs-theme="dark">
                       <Container className="nav">
-                          <Navbar.Brand href="#home">Medical</Navbar.Brand>
+                          <Navbar.Brand href="App.jsx">Medical</Navbar.Brand>
                           <Navbar.Toggle aria-controls="basic-navbar-nav" />
                           <Navbar.Collapse id="basic-navbar-nav">
-                              <Nav className="me-auto ms-auto">
-                                  <NavLink to="/">Home</NavLink>
-                                  <NavLink to="/">About</NavLink>
-                                  <NavLink to="/">Service</NavLink>
-                                  <NavLink to="/">Contact</NavLink>
-                                  <NavLink to="/RegisterForm">Register</NavLink>
-                                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                      <NavDropdown.Item href="#action/3.1">Services</NavDropdown.Item>
-                                      <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                          
 
-                                  </NavDropdown>
-                                  <Button style={{ marginLeft: '290px' }} variant="outline-success">Make an appointment</Button>{' '}
-                              </Nav>
+                              <ListGroup horizontal className="NavList">
+                                  <ListGroup.Item className="item"  action variant="success"  href="App.jsx">Home</ListGroup.Item>
+                                  <ListGroup.Item  variant="success"action href="#About" target="_blank">About</ListGroup.Item>
+                                      <ListGroup.Item action variant="success"  href="#Clinic" >Clinic</ListGroup.Item>
+                                      <ListGroup.Item variant="success" action href="#Doctors" >Doctors</ListGroup.Item>
+                                      <ListGroup.Item variant="success" action href="#Contact" >Contact</ListGroup.Item>
+                                      <ListGroup.Item variant="success" action href="#Register" >Register</ListGroup.Item>
+                                  </ListGroup>
+
+
+                              <Button style={{ marginLeft: '290px' }} variant="outline-success">Make an appointment</Button>{' '}
+                            
                           </Navbar.Collapse>
                       </Container>
                   </Navbar>
               </header>
-              <IntervalSlider images={images} interval={3000} />
+              <Ballina/>
+              <IntervalSlider images={images} interval={3000}/>
               <Card />
               <Footer />
           </div>
--- reactapp1.client/src/Ballina.jsx
+++ reactapp1.client/src/Ballina.jsx
@@ -1,13 +1,20 @@
-import  Button from 'react-bootstrap/Button';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 import image1 from './assets/Ballina.png';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
 function Ballina() {
+    const cities = ['Prishtine', 'Gjilan', 'Ferizaj', 'Peje'];
     return (  
-        <div>
-            <img src={image1} style={{ width: '100%', margin: '-1px' }}></img>
+        <>
+            <Image src={image1} fluid style={{ width: '100%', margin: '-1px' }} ></Image>
             <Button className="service" style={{ margin: '-220px 0px 290px 236px', background: '#234820' }}> Take a Service</Button>
-        </div>
+            <ListGroup horizontal>
+                {cities.map((item) => <ListGroup.Item action variant="danger"   key={item}>{item}</ListGroup.Item>)}
+            </ListGroup>
+
+        </>
     );
 
 }
