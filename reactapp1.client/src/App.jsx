cd3f6ad86c3ba078bf6bfc294d6238ccb406cf69
meriton <137052060+meriton41@users.noreply.github.com>
2024-04-07 16:13:53 +0200

    Krijimi i Register form.

--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -1,22 +1,64 @@
-import React from 'react';
-import Header from "./Header";
+import React, { Component } from 'react';
 import Footer from "./Footer";
 import Card from "./Card";
 import IntervalSlider from './IntervalSlider';
 import i2 from './assets/i2.jpg';
 import i3 from './assets/i3.jpg';
-import i4 from './assets/i4.jpg';
-function App() {
-    const images = [i3, i2, i4];
-    return (
-        <div>
-            <Header />
-            <IntervalSlider images={images} interval={3000} />
-            <Card />
-            <Footer />
-        </div>
+import i4 from './assets/i4.jpg'; import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, NavLink, Routes, HashRouter } from 'react-router-dom';
+const RegisterForm = React.lazy(() => import("./RegisterForm"));
+  const Header = () => {
+      const images = [i3, i2, i4];
+      return (
+          <div>
+              <header>
+                  <Navbar bg="dark" data-bs-theme="dark">
+                      <Container className="nav">
+                          <Navbar.Brand href="#home">Medical</Navbar.Brand>
+                          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                          <Navbar.Collapse id="basic-navbar-nav">
+                              <Nav className="me-auto ms-auto">
+                                  <NavLink to="/">Home</NavLink>
+                                  <NavLink to="/">About</NavLink>
+                                  <NavLink to="/">Service</NavLink>
+                                  <NavLink to="/">Contact</NavLink>
+                                  <NavLink to="/RegisterForm">Register</NavLink>
+                                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                      <NavDropdown.Item href="#action/3.1">Services</NavDropdown.Item>
+                                      <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+
+                                  </NavDropdown>
+                                  <Button style={{ marginLeft: '290px' }} variant="outline-success">Make an appointment</Button>{' '}
+                              </Nav>
+                          </Navbar.Collapse>
+                      </Container>
+                  </Navbar>
+              </header>
+              <IntervalSlider images={images} interval={3000} />
+              <Card />
+              <Footer />
+          </div>
+      );
+             
+}
+class App extends Component {
+    render() {
+    return( 
+            <div>
+                <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Header />}></Route>
+                        <Route path="/RegisterForm" element={<RegisterForm />}></Route>
+                    </Routes>
+                </HashRouter >
+               </div>
     );
+        }
 }
 
-
-export default App;
+export default App; 
--- reactapp1.client/src/Header.jsx
+++ reactapp1.client/src/Header.jsx
@@ -1,37 +1,45 @@
+import React, { Suspense } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RegisterForm from './RegisterForm.jsx';
+import { Route, NavLink, Routes, HashRouter } from 'react-router-dom';
+const RegisterForm = React.lazy(() => import("./RegisterForm"));
 function Header() {
-
+    
     return (
+        <>
         <header>
             <Navbar bg="dark" data-bs-theme="dark">
-      <Container className="nav">
-        <Navbar.Brand href="#home">Medical</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto ms-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">About</Nav.Link>
-                            <Nav.Link href="#services">Services</Nav.Link>
-                            <Nav.Link to '/RegisterForm.jsx'>Register</Nav.Link>
-                            <Nav.Link>Contact</Nav.Link>
-           
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Services</NavDropdown.Item>
+                <Container className="nav">
+                    <Navbar.Brand href="#home">Medical</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto ms-auto">
+                            <NavLink to="/">Home</NavLink>
+                            <NavLink to="/">About</NavLink>
+                            <NavLink to="/RegisterForm">Services</NavLink>
+                            <NavLink to="/">Contact</NavLink>
+
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Services</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              
-            </NavDropdown>
+
+                            </NavDropdown>
                             <Button style={{ marginLeft: '290px' }} variant="outline-success">Make an appointment</Button>{' '}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
+        <HashRouter>
+          <Routes>
+             <Route path="/RegisterForm" element={<RegisterForm />}></Route>
+          </Routes>
+        </HashRouter>
+        </>
     );
 }
-export default Header;
+export default Header; 
