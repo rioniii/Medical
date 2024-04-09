3d3d7f57c3ae0eed4aeb11ec84e075ebe9766531
rioniii <148160573+rioniii@users.noreply.github.com>
2024-04-09 20:27:54 +0200

    Shtimi i map (hartes ne footer)

--- reactapp1.client/src/Footer.jsx
+++ reactapp1.client/src/Footer.jsx
@@ -34,6 +34,7 @@ function Footer(){
                             <a href="#" ><li>Company</li> </a>
                         </ul>
 
+
                             
                                 <a
                                     href="https://www.youtube.com/c/jamesqquick"
@@ -55,7 +56,16 @@ function Footer(){
                                 </a>
                                 <a href="https://wwww.twitter.com" className="twitter social">
                                     <FontAwesomeIcon icon={faTwitter} size="2x" />
-                                </a>
+                                </a><br></br><br></br><br></br>
+
+                                <div className='col-lg-5'>
+                                    <iframe
+                                        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12094.57348593182!2d-74.00599512526003!3d40.72586666928451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2598f988156a9%3A0xd54629bdf9d61d68!2sBroadway-Lafayette%20St!5e0!3m2!1spl!2spl!4v1624523797308!5m2!1spl!2spl'
+                                        className='h-100 w-100'
+                                        style={{ border: '0' }}
+                                        loading='lazy'
+                                    ></iframe>
+                                </div>
 
                     </div>
 
