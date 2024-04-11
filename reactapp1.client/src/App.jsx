5cb3a85b0e12ac10b346cf29e7a53beba21e6238
meriton <137052060+meriton41@users.noreply.github.com>
2024-04-11 22:17:53 +0200

    NavBar

--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -12,8 +12,6 @@ import { Route, Routes, HashRouter } from 'react-router-dom';
 import i4 from './assets/i4.jpg';
 import i2 from './assets/i2.jpg';
 import i3 from './assets/i3.jpg';
-import logo from 'C:\Users\W11\source\repos\Medical\reactapp1.client\src\assets\medical care logo template social media .png';
-
 
 const RegisterForm = React.lazy(() => import("./RegisterForm"));
 const LoginForm = React.lazy(() => import("./LoginForm"));
@@ -25,45 +23,35 @@ const images = [i3, i2, i4];
       return (
           <div>
               <header>
-                  <Navbar bg="dark" data-bs-theme="dark">
-                      <Container className="nav">
-                          <Navbar.Brand href="App.jsx">
-                              <img
-                                  src={logo}
-                                  alt="Your Logo"
-                                  height="30"
-                                  className="d-inline-block align-top"
-                              />
+                  <Navbar bg="dark" variant="dark" expand="lg">
+                      <Container fluid>
+                          <Navbar.Brand href="/">
+
+                              Medical
                           </Navbar.Brand>
-                          <Navbar.Brand href="App.jsx">Medical</Navbar.Brand>
                           <Navbar.Toggle aria-controls="basic-navbar-nav" />
                           <Navbar.Collapse id="basic-navbar-nav">
-                          
-
-                              <ListGroup horizontal className="NavList">
-                                  <ListGroup.Item className="item"  action variant="success"  href="App.jsx">Home</ListGroup.Item>
-                                  <ListGroup.Item  variant="success"action href="#About" target="_blank">About</ListGroup.Item>
-                                      <ListGroup.Item action variant="success"  href="#Clinic" >Clinic</ListGroup.Item>
-                                      <ListGroup.Item variant="success" action href="#Doctors" >Doctors</ListGroup.Item>
-                                      <ListGroup.Item variant="success" action href="#Contact" >Contact</ListGroup.Item>
-                                      <ListGroup.Item variant="success" action href="#RegisterForm" >Register</ListGroup.Item>
-                                  </ListGroup>
-
-
-                              <Button variant="success"   style={{ marginLeft: '290px'}} >Make an appointment</Button>{' '}
-                            
+                              <ListGroup className="NavList mb-2 mb-lg-0" horizontal="lg">
+                                  <ListGroup.Item className="item" action variant="success" href="/">Home</ListGroup.Item>
+                                  <ListGroup.Item variant="success" action href="#About">About</ListGroup.Item>
+                                  <ListGroup.Item variant="success" action href="#Clinic">Clinic</ListGroup.Item>
+                                  <ListGroup.Item variant="success" action href="#Doctors">Doctors</ListGroup.Item>
+                                  <ListGroup.Item variant="success" action href="#Contact">Contact</ListGroup.Item>
+                                  <ListGroup.Item variant="success" action href="#RegisterForm">Register</ListGroup.Item>
+                              </ListGroup>
+                              <Button variant="success" className="ms-lg-2">Make an appointment</Button>
                           </Navbar.Collapse>
                       </Container>
                   </Navbar>
-              </header>
-              <Ballina/>
-              <IntervalSlider images={images} interval={3000}/>
-              <Card />
-              <Footer/>
-          </div>
-      );
-             
+                  </header>
+                  <Ballina />
+                  <IntervalSlider images={images} interval={3000} />
+                  <Card />
+                  <Footer />
+              </div>
+              );
 }
+
 class App extends Component {
     render() {
     return( 
