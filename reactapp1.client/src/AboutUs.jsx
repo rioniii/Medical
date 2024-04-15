31a93a80005caf9b711be79a651173c3a6c9d78e
ilmiAliu <ilmialiu138@gmail.com>
2024-04-15 18:23:22 +0200

    konteksti i About us  dhe pikat kryesore te tij

---
+++ reactapp1.client/src/AboutUs.jsx
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function AboutUs  () {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>About Us</h1>
+                        <h2>  Welcome to Medical </h2>
+                        <h3>
+                            where we revolutionize hospital management with cutting-edge technology and a commitment to excellence. Our team of skilled developers and designers harnesses the power of React.js and Bootstrap to create intuitive and efficient solutions tailored specifically for the healthcare industry.
+                        </h3>
+                       
+                        <h5>
+                        
+                           
+                            Our Mission
+                            At Meidal, our mission is simple: to streamline hospital operations and improve patient care through innovative software solutions. We believe that every hospital deserves access to advanced management tools that enhance efficiency, productivity, and overall performance.
+
+                            Why Choose Us?
+                            Seamless Integration
+                            Our management system seamlessly integrates into existing hospital infrastructure, minimizing disruption and maximizing effectiveness from day one.
+
+                            Customization
+                            We understand that every hospital is unique. That's why our solutions are highly customizable, allowing you to tailor the system to meet the specific needs and workflows of your institution.
+
+                            Real-time Insights
+                            Gain invaluable insights into hospital operations with our real-time analytics dashboard. Monitor key performance metrics, identify trends, and make data-driven decisions to optimize efficiency and resource allocation.
+
+                            Enhanced Patient Care
+                            Our user-friendly interface empowers healthcare professionals to spend less time on administrative tasks and more time delivering quality care to patients. From appointment scheduling to electronic health records management, we simplify processes to prioritize what matters most—patient well-being.
+
+                            Security and Compliance
+                            Protect patient data and ensure compliance with industry regulations with our robust security measures. Rest easy knowing that sensitive information is safeguarded against unauthorized access, breaches, and cyber threats.
+
+                            Ongoing Support
+                            Our commitment to your success doesn't end after implementation. Our dedicated support team is here to assist you every step of the way, providing ongoing training, troubleshooting, and updates to ensure optimal performance and satisfaction.
+
+                            Let's Transform Healthcare Together
+                            Join us in reshaping the future of healthcare management. Contact us today to learn more about our innovative solutions and how we can tailor them to meet the unique needs of your hospital.
+                        </h5>
+                    </Col>
+                </Row>
+            </Container>
+        );
+   
+
+
+}
+
+export default AboutUs;
+
+
--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -15,6 +15,7 @@ import i3 from './assets/i3.jpg';
 
 const RegisterForm = React.lazy(() => import("./RegisterForm"));
 const LoginForm = React.lazy(() => import("./LoginForm"));
+const AboutUs = React.lazy(() => import("./AboutUs"));
 const Header = () => {
 const images = [i3, i2, i4];
 
@@ -33,7 +34,7 @@ const images = [i3, i2, i4];
                           <Navbar.Collapse id="basic-navbar-nav">
                               <ListGroup className="NavList mb-2 mb-lg-0" horizontal="lg">
                                   <ListGroup.Item className="item" action variant="success" href="/">Home</ListGroup.Item>
-                                  <ListGroup.Item variant="success" action href="#About">About</ListGroup.Item>
+                                  <ListGroup.Item variant="success" action href="#AboutUs">About Us</ListGroup.Item>
                                   <ListGroup.Item variant="success" action href="#Clinic">Clinic</ListGroup.Item>
                                   <ListGroup.Item variant="success" action href="#Doctors">Doctors</ListGroup.Item>
                                   <ListGroup.Item variant="success" action href="#Contact">Contact</ListGroup.Item>
@@ -61,6 +62,8 @@ class App extends Component {
                     <Route path="/" element={<Header />}></Route>
                     <Route path="/RegisterForm" element={<RegisterForm />}></Route>
                     <Route path="/LoginForm" element={<LoginForm />}></Route>
+                    <Route path="/AboutUs" element={<AboutUs />}></Route>
+
                     </Routes>
                 </HashRouter >
         </Suspense>
