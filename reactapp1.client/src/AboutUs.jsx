46f948838ec8e56a535cc0c131d791d9e502be13
ilmiAliu <ilmialiu138@gmail.com>
2024-04-17 18:22:04 +0200

    perditsime te pergjithsme ne about us

--- reactapp1.client/src/AboutUs.jsx
+++ reactapp1.client/src/AboutUs.jsx
@@ -16,7 +16,9 @@ function AboutUs  () {
                 {/* Text content */}
                 <div className={ChangeVideo.content}>
                     <h1>About Us</h1>
+
                     <h2>Welcome to Medical</h2>
+
                     <h3>Where we revolutionize hospital management with cutting-edge technology and a commitment to excellence.</h3>
                 </div>
             </div>
@@ -32,33 +34,33 @@ function AboutUs  () {
                     <br></br>
             <br></br>
 
-                            Why Choose Us?
-                            Seamless Integration
+                    Why Choose Us?<br></br>
+                    Seamless Integration<br></br>
                             Our management system seamlessly integrates into existing hospital infrastructure,<br/> minimizing disruption and maximizing effectiveness from day one.
                     <br></br>
                     <br></br>
                    
-                            Customization
+                    Customization<br></br>
                             We understand that every hospital is unique. That's why our solutions are highly customizable, allowing you to tailor the system to meet the specific needs and workflows of your institution.
                     <br></br>
                     <br></br>
-                            Real-time Insights
+                            Real-time Insights<br></br>
                             Gain invaluable insights into hospital operations with our real-time analytics dashboard. Monitor key performance metrics, identify trends, and make data-driven decisions to optimize efficiency and resource allocation.
                     <br></br>
                     <br></br>
-                            Enhanced Patient Care
+                    Enhanced Patient Care<br></br>
                             Our user-friendly interface empowers healthcare professionals to spend less time on administrative tasks and more time delivering quality care to patients. From appointment scheduling to electronic health records management, we simplify processes to prioritize what matters most patient well-being.
                     <br></br>
                     <br></br>
-                            Security and Compliance
+                    Security and Compliance<br></br>
                             Protect patient data and ensure compliance with industry regulations with our robust security measures. Rest easy knowing that sensitive information is safeguarded against unauthorized access, breaches, and cyber threats.
                     <br></br>
                     <br></br>
-                            Ongoing Support
+                    Ongoing Support<br></br>
                             Our commitment to your success doesn't end after implementation. Our dedicated support team is here to assist you every step of the way, providing ongoing training, troubleshooting, and updates to ensure optimal performance and satisfaction.
                     <br></br>
                     <br></br>
-                            Let's Transform Healthcare Together
+                    Let's Transform Healthcare Together<br></br>
                             Join us in reshaping the future of healthcare management. Contact us today to learn more about our innovative solutions and how we can tailor them to meet the unique needs of your hospital.
                         </h5>
                    </div>
