9bb5b49024313f24062df010e018592bc82b053c
meriton <137052060+meriton41@users.noreply.github.com>
2024-04-22 11:54:53 +0200

    Update i atributeve

--- ReactApp1.Server/Data/Models/Repart.cs
+++ ReactApp1.Server/Data/Models/Repart.cs
@@ -4,14 +4,14 @@
     {
 
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int kati { get; set; }
-        public int NrDhomave { get; set; }
-        public string LlojRepartit { get; set; }
+        public string? Name { get; set; }
+        public int? kati { get; set; }
+        public int? NrDhomave { get; set; }
+        public string? LlojRepartit { get; set; }
 
-        public int NrDoktoreve { get; set; }
+        public int? NrDoktoreve { get; set; }
 
-        public int NrAssistenteve { get; set; }
+        public int? NrAssistenteve { get; set; }
 
     }
 }
--- ReactApp1.Server/Data/Models/Staf.cs
+++ ReactApp1.Server/Data/Models/Staf.cs
@@ -5,14 +5,14 @@
         public int Id { get; set; }
         public string? Emri { get; set; }    
         public string? Mbiemri { get; set; }
-        public int Mosha { get; set; }
+        public int? Mosha { get; set; }
         public string? Qyteti { get; set; } 
         public string? NrTelefonit { get; set; } 
         public string? NiveliEdukimit { get; set; }
         public string? Specializimi { get; set; }
         public string? Roli { get; set; }
         public string? Nderimi { get; set; }
-        public double Paga { get; set; }
+        public double? Paga { get; set; }
         public string? Image { get; set; } = string.Empty;
     }
 }
