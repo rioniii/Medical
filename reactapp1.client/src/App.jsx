dcfa4a914f3144e65003487bbb6cd85ff43bfdb3
meriton <137052060+meriton41@users.noreply.github.com>
2024-04-24 13:35:14 +0200

    ..

--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -26,37 +26,38 @@ const Header = () => {
 
 
 
-      return (
-          <div>
-              <header>
-                  <Navbar bg="dark" variant="dark" expand="lg">
-                      <Container fluid>
-                          <Navbar.Brand href="/">
+    return (
+        <div>
+            <header>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Container fluid>
+                        <Navbar.Brand href="/">
 
-                                Medical
-                          </Navbar.Brand>
-                          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                          <Navbar.Collapse id="basic-navbar-nav">
-                              <ListGroup className="NavList mb-2 mb-lg-0" horizontal="lg">
-                                  <ListGroup.Item className="item" action variant="success" href="/">Home</ListGroup.Item>
-                                  <ListGroup.Item variant="success" action href="#AboutUs">About Us</ListGroup.Item>
-                                  <ListGroup.Item variant="success" action href="#Clinic">Clinic</ListGroup.Item>
-                                  <ListGroup.Item variant="success" action href="#Doctors">Doctors</ListGroup.Item>
-                                  <ListGroup.Item variant="success" action href="#Contact">Contact</ListGroup.Item>
-                                  <ListGroup.Item variant="success" action href="#RegisterForm">Register</ListGroup.Item>
-                                  <ListGroup.Item variant="success" action href="#PatientCRUD">Dashboard</ListGroup.Item>
-                              </ListGroup>
-                              <Button variant="success" className="ms-lg-2">Make an appointment</Button>
-                          </Navbar.Collapse>
-                      </Container>
-                  </Navbar>
-                  </header>
-              <Ballina />
-              <PatientCRUD />
-                  <IntervalSlider images={images} interval={3000} />
-                  <Card />
-                  <Footer />
-              </div>
+                            Medical
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <ListGroup className="NavList mb-2 mb-lg-0" horizontal="lg">
+                                <ListGroup.Item className="item" action variant="success" href="/">Home</ListGroup.Item>
+                                <ListGroup.Item variant="success" action href="#AboutUs">About Us</ListGroup.Item>
+                                <ListGroup.Item variant="success" action href="#Clinic">Clinic</ListGroup.Item>
+                                <ListGroup.Item variant="success" action href="#Doctors">Doctors</ListGroup.Item>
+                                <ListGroup.Item variant="success" action href="#Contact">Contact</ListGroup.Item>
+                                <ListGroup.Item variant="success" action href="#RegisterForm">Register</ListGroup.Item>
+                                <ListGroup.Item variant="success" action href="#PatientCRUD">Dashboard</ListGroup.Item>
+                            </ListGroup>
+                            <Button variant="success" className="ms-lg-2">Make an appointment</Button>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+            <Ballina />
+            <PatientCRUD />
+            <IntervalSlider images={images} interval={3000} />
+            <Card />
+            <Footer />
+        </div>
+    );
               
 }
 
