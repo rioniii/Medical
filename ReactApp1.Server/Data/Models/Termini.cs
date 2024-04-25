05122503798891cd3206ce9421859116f64a7b4d
ubejdaliu06 <ua68003@ubt-uni.net>
2024-04-25 11:39:10 +0200

    Termin.cs

---
+++ ReactApp1.Server/Data/Models/Termini.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Identity.Client;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class Termini
+    {
+        public int Termini_Id { get; }
+
+        public string Statusi { get; set; }
+         public int Data { get; set; }
+
+        public Pacienti Pacienti { get; set; }
+    ?
+
+            
+     
+
+    }
+}
