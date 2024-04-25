b94b038b9847624c898e44528455afef26eb27ac
rioniii <148160573+rioniii@users.noreply.github.com>
2024-04-25 14:16:30 +0200

    Pershkrimi.cs

---
+++ ReactApp1.Server/Data/Models/Pershkrimi.cs
@@ -0,0 +1,14 @@
+﻿namespace ReactApp1.Server.Data.Models
+{
+    public class Pershkrimi
+    {
+        public int Pershkrimi_Id { get; set; }
+        public string Anamneza_Statusi { get; set; }
+        public string Ekzaminimi { get; set; }
+        public string Diagnoza {  get; set; }
+        public string Terapia { get; set; }
+        public string Perfundimi { get; set; }
+
+        public 
+    }
+}
