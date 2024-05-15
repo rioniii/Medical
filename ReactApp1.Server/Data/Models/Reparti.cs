9150e547451ed981557151ad0a306b8ea70c8673
ilmiAliu <ilmialiu138@gmail.com>
2024-05-15 20:13:55 +0200

    Modelet edhe Controllerat

---
+++ ReactApp1.Server/Data/Models/Reparti.cs
@@ -0,0 +1,19 @@
+﻿namespace ReactApp1.Server.Data.Models
+{
+    public class Reparti
+    {
+        public int Reparti_Id { get; }
+
+        public string Emri{ get; set; }
+        public int Kati{ get; set; }
+        public int NrDhomave{ get; set; }
+        public int Mjeku_Id{ get; set; }
+        //public List<Mjeku> Mjeket { get; set; }
+       
+        public int Dhoma_Id { get;}
+       // public List<Dhoma> Dhomat { get; set; }
+
+        //public List<Reparti> Repartet { get; set; }
+    }
+}
+
