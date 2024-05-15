13f5ddf7e726debe37832b54ff10a60201557ded
meriton <137052060+meriton41@users.noreply.github.com>
2024-05-15 14:12:35 +0200

    .

--- ReactApp1.Server/Controllers/ContactController.cs
+++ ReactApp1.Server/Controllers/ContactController.cs
@@ -8,62 +8,63 @@ namespace ReactApp1.Server.Controllers
     [ApiController]
     public class ContactController : ControllerBase
     {
-            public readonly AppDBContext _context;
+        public readonly AppDBContext _context;
 
-            public ContactController(AppDBContext context)
-            {
-                _context = context;
-            }
+        public ContactController(AppDBContext context)
+        {
+            _context = context;
+        }
 
-            [HttpGet]
-            public async Task<ActionResult<List<Contact>>> GetAllFaturat()
-            {
-                var contacti = await _context.Contacti.ToListAsync();
+        [HttpGet]
+        public async Task<ActionResult<List<Contact>>> GetAllFaturat()
+        {
+            var contacti = await _context.Contacti.ToListAsync();
 
-                return Ok(contacti);
-            }
-
-
-            [HttpGet("{id}")]
-            public async Task<ActionResult<List<Contact>>> GetContacti(int id)
-            {
-                var contacti = await _context.Contacti.FindAsync(id);
-                if (contacti == null)
-                    return NotFound("Contacti not found");
-                return Ok(contacti);
-            }
-
-            [HttpPost]
-            public async Task<ActionResult<List<Contact>>> AddContact(Contact contacti)
-            {
-                _context.Contacti.Add(contacti);
-                await _context.SaveChangesAsync();
-
-                return Ok(await _context.Contacti.ToListAsync()); ;
-            }
+            return Ok(contacti);
+        }
 
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Contact>>> GetContacti(int id)
+        {
+            var contacti = await _context.Contacti.FindAsync(id);
+            if (contacti == null)
+                return NotFound("Contacti not found");
+            return Ok(contacti);
+        }
 
-            [HttpPatch]
-            [Route("UpdateContacti/{id}")]
-            public async Task<Contact> UpdateContact(Contact objContacti)
-            {
-                _context.Entry(objContacti).State = EntityState.Modified;
-                await _context.SaveChangesAsync();
-                return objContacti;
-            }
+        [HttpPost]
+        public async Task<ActionResult<List<Contact>>> AddContact(Contact contacti)
+        {
+            _context.Contacti.Add(contacti);
+            await _context.SaveChangesAsync();
 
-            [HttpDelete("{id}")]
-            public async Task<ActionResult<List<Contact>>> DeleteContacti(int id)
-            {
-                var dbContacti = await _context.Contacti.FindAsync(id);
-                if (dbContacti == null)
-                    return NotFound("Contacti not found");
+            return Ok(await _context.Contacti.ToListAsync()); ;
+        }
 
-                _context.Contacti.Remove(dbContacti);
 
-                await _context.SaveChangesAsync();
 
-                return Ok(await _context.Contacti.ToListAsync()); ;
-            }
+        [HttpPatch]
+        [Route("UpdateContacti/{id}")]
+        public async Task<Contact> UpdateContact(Contact objContacti)
+        {
+            _context.Entry(objContacti).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return objContacti;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Contact>>> DeleteContacti(int id)
+        {
+            var dbContacti = await _context.Contacti.FindAsync(id);
+            if (dbContacti == null)
+                return NotFound("Contacti not found");
+
+            _context.Contacti.Remove(dbContacti);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Contacti.ToListAsync()); ;
+        }
+    }
 }
