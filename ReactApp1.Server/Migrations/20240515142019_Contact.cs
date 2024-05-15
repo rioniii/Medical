3632fb46452b9b1efe5118c1f74fdd7df0157461
meriton <137052060+meriton41@users.noreply.github.com>
2024-05-15 16:21:03 +0200

    Krijimi i tabeles COntact ne databaze.

---
+++ ReactApp1.Server/Migrations/20240515142019_Contact.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReactApp1.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Contact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacti",
+                columns: table => new
+                {
+                    Contact_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacti", x => x.Contact_Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacti");
+        }
+    }
+}
