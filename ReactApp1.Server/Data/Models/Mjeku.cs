441b52df50719c206b790d5ac26051c295ebb292
ilmiAliu <ilmialiu138@gmail.com>
2024-05-29 15:31:28 +0200

    Mjeku

---
+++ ReactApp1.Server/Data/Models/Mjeku.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class Mjeku
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Mjeku_Id { get; set; }
+
+        public string UserId { get; set; }  
+        public ApplicationUser User { get; set; }
+
+        [Required(ErrorMessage = "Reparti is required")]
+        public string Reparti { get; set; }
+
+        [Required(ErrorMessage = "Nderrimi is required")]
+        public String Nderrimi { get; set; }
+
+        [Required(ErrorMessage = "Angazhimi is required")]
+        public String Angazhimi { get; set; }
+
+
+        
+        
+    }
+}
