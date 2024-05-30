48f9996e0852aef2240d4788c2914d85c36e445e
ilmiAliu <ilmialiu138@gmail.com>
2024-05-30 14:33:28 +0200

    .

--- reactapp1.client/src/Dashboard/MjekuCRUD.jsx
+++ reactapp1.client/src/Dashboard/MjekuCRUD.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import axios from 'axios';
 import { Button, Modal, Table, Form } from 'react-bootstrap';
 
@@ -57,69 +58,71 @@ const MjekuCRUD = () => {
         }
     };
 
+
     return (
         <>
             <Button onClick={() => handleShow()}>Add New Doctor</Button>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Emri</th>
-                        <th>Mbiemri</th>
-                        <th>Reparti</th>
-                        <th>Specializimi</th>
-                        <th>Nderrimi</th>
-                        <th>Angazhimi</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {doctors.map(doctor => (
-                        <tr key={doctor.id}>
-                            <td>{doctor.emri}</td>
-                            <td>{doctor.mbiemri}</td>
-                            <td>{doctor.reparti}</td>
-                            <td>{doctor.specializimi}</td>
-                            <td>{doctor.nderrimi}</td>
-                            <td>{doctor.angazhimi}</td>
-                            <td>
-                                <Button variant="warning" onClick={() => handleShow(doctor)}>Edit</Button>
-                                <Button variant="danger" onClick={() => handleDelete(doctor.id)}>Delete</Button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-
+            <Container>
+                {doctors.map(doctor => (
+                    <Row key={doctor.id} className="mb-3">
+                        <Col>{doctor.emri}</Col>
+                        <Col>{doctor.mbiemri}</Col>
+                        <Col>{doctor.reparti}</Col>
+                        <Col>{doctor.specializimi}</Col>
+                        <Col>{doctor.nderrimi}</Col>
+                        <Col>{doctor.angazhimi}</Col>
+                        <Col>
+                            <Button variant="warning" onClick={() => handleShow(doctor)}>Edit</Button>
+                            <Button variant="danger" onClick={() => handleDelete(doctor.id)}>Delete</Button>
+                        </Col>
+                    </Row>
+                ))}
+            </Container>
+    
             <Modal show={showModal} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>{currentDoctor.id ? 'Edit Doctor' : 'Add Doctor'}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <Form.Group>
-                            <Form.Label>Emri</Form.Label>
-                            <Form.Control type="text" name="emri" value={currentDoctor.emri} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Mbiemri</Form.Label>
-                            <Form.Control type="text" name="mbiemri" value={currentDoctor.mbiemri} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Reparti</Form.Label>
-                            <Form.Control type="text" name="reparti" value={currentDoctor.reparti} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Specializimi</Form.Label>
-                            <Form.Control type="text" name="specializimi" value={currentDoctor.specializimi} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Nderrimi</Form.Label>
-                            <Form.Control type="text" name="nderrimi" value={currentDoctor.nderrimi} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Angazhimi</Form.Label>
-                            <Form.Control type="text" name="angazhimi" value={currentDoctor.angazhimi} onChange={handleChange} />
-                        </Form.Group>
+                        <Row>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Emri</Form.Label>
+                                    <Form.Control type="text" name="emri" value={currentDoctor.emri} onChange={handleChange} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Mbiemri</Form.Label>
+                                    <Form.Control type="text" name="mbiemri" value={currentDoctor.mbiemri} onChange={handleChange} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Reparti</Form.Label>
+                                    <Form.Control type="text" name="reparti" value={currentDoctor.reparti} onChange={handleChange} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Specializimi</Form.Label>
+                                    <Form.Control type="text" name="specializimi" value={currentDoctor.specializimi} onChange={handleChange} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Nderrimi</Form.Label>
+                                    <Form.Control type="text" name="nderrimi" value={currentDoctor.nderrimi} onChange={handleChange} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Angazhimi</Form.Label>
+                                    <Form.Control type="text" name="angazhimi" value={currentDoctor.angazhimi} onChange={handleChange} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
@@ -129,6 +132,5 @@ const MjekuCRUD = () => {
             </Modal>
         </>
     );
-};
-
+  }
 export default MjekuCRUD;
