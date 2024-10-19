cb961e6a1a34beb108f73c234043e07c837e02dc
Rion Hoxha <rionhoxha123@gmail.com>
2024-10-19 23:27:45 +0200

    Termini Controller

---
+++ ReactApp1.Server/Controllers/TerminiController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactApp1.Server.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TerminiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TerminiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Termini
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Termini>>> GetTerminet()
+        {
+            return await _context.Terminet
+                .Include(t => t.Mjeku)
+                .Include(t => t.Pacienti)
+                .ToListAsync();
+        }
+
+        // GET: api/Termini/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Termini>> GetTermini(int id)
+        {
+            var termini = await _context.Terminet
+                .Include(t => t.Mjeku)
+                .Include(t => t.Pacienti)
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (termini == null)
+            {
+                return NotFound();
+            }
+
+            return termini;
+        }
+
+        // PUT: api/Termini/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTermini(int id, Termini termini)
+        {
+            if (id != termini.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(termini).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TerminiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Termini
+        [HttpPost]
+        public async Task<ActionResult<Termini>> PostTermini(Termini termini)
+        {
+            _context.Terminet.Add(termini);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTermini", new { id = termini.Id }, termini);
+        }
+
+        // DELETE: api/Termini/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTermini(int id)
+        {
+            var termini = await _context.Terminet.FindAsync(id);
+            if (termini == null)
+            {
+                return NotFound();
+            }
+
+            _context.Terminet.Remove(termini);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TerminiExists(int id)
+        {
+            return _context.Terminet.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ReactApp1.Server/Migrations/20241019212527_TerminiController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ReactApp1.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class TerminiController : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "10b68af0-245f-43db-9795-80dd44399958");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cca31275-44de-4a57-a76e-6c82ffcb8766");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "edef91e5-8a87-4841-ba15-b8653715bc3a");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0a64feca-8bd6-48fc-a3b6-7ea41647822e", "2", "User", "User" },
+                    { "63fd1c23-2419-4e40-8fff-8a01d915a5e1", "3", "Doctor", "Doctor" },
+                    { "755f7643-5dbd-404f-98ae-cc3bf55ec148", "1", "Admin", "Admin" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0a64feca-8bd6-48fc-a3b6-7ea41647822e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "63fd1c23-2419-4e40-8fff-8a01d915a5e1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "755f7643-5dbd-404f-98ae-cc3bf55ec148");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "10b68af0-245f-43db-9795-80dd44399958", "3", "Doctor", "Doctor" },
+                    { "cca31275-44de-4a57-a76e-6c82ffcb8766", "2", "User", "User" },
+                    { "edef91e5-8a87-4841-ba15-b8653715bc3a", "1", "Admin", "Admin" }
+                });
+        }
+    }
+}
