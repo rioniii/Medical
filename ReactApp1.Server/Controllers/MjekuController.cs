b1c03cb8e712f2b034a04c176b199a03bdb1f7f3
ubejdaliu06 <ua68003@ubt-uni.net>
2024-10-21 02:25:25 +0200

    Migration Mjeku

---
+++ ReactApp1.Server/Controllers/MjekuController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactApp1.Server.Data;
+using ReactApp1.Server.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MjekuController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MjekuController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Mjeku
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Mjeku>>> GetMjeket()
+        {
+            return await _context.Mjeket
+                .Include(m => m.User)           // Include related User data
+                .Include(m => m.Terminet)       // Include related Termini (Appointments)
+                .Include(m => m.Historiks)      // Include related Historiks (History)
+                .ToListAsync();
+        }
+
+        // GET: api/Mjeku/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Mjeku>> GetMjeku(int id)
+        {
+            var mjeku = await _context.Mjeket
+                .Include(m => m.User)           // Include related User data
+                .Include(m => m.Terminet)       // Include related Termini (Appointments)
+                .Include(m => m.Historiks)      // Include related Historiks (History)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (mjeku == null)
+            {
+                return NotFound();
+            }
+
+            return mjeku;
+        }
+
+        // PUT: api/Mjeku/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMjeku(int id, Mjeku mjeku)
+        {
+            if (id != mjeku.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(mjeku).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MjekuExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Mjeku
+        [HttpPost]
+        public async Task<ActionResult<Mjeku>> PostMjeku(Mjeku mjeku)
+        {
+            // Make sure to add foreign key relations (User)
+            var user = await _context.Users.FindAsync(mjeku.UserId);
+            if (user == null)
+            {
+                return BadRequest("Invalid UserId provided.");
+            }
+
+            _context.Mjeket.Add(mjeku);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMjeku", new { id = mjeku.Id }, mjeku);
+        }
+
+        // DELETE: api/Mjeku/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMjeku(int id)
+        {
+            var mjeku = await _context.Mjeket.FindAsync(id);
+            if (mjeku == null)
+            {
+                return NotFound();
+            }
+
+            _context.Mjeket.Remove(mjeku);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MjekuExists(int id)
+        {
+            return _context.Mjeket.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ReactApp1.Server/Migrations/20241021002456_Mjeku.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ReactApp1.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mjeku : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Historiqet_Mjeket_MjekuId",
+                table: "Historiqet");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Historiqet_Pacientet_PacientId",
+                table: "Historiqet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Historiqet",
+                table: "Historiqet");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "464a115c-8dff-426b-a5f4-24530fbc8dcf");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8aef10f4-ac67-41a2-8048-1f037c660774");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b069e9ec-8bc0-4350-9041-3ba8fb4aef0b");
+
+            migrationBuilder.RenameTable(
+                name: "Historiqet",
+                newName: "Historiks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Historiqet_PacientId",
+                table: "Historiks",
+                newName: "IX_Historiks_PacientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Historiqet_MjekuId",
+                table: "Historiks",
+                newName: "IX_Historiks_MjekuId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Historiks",
+                table: "Historiks",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "16e9155a-f42f-4897-a913-b7f516792b61", "3", "Doctor", "Doctor" },
+                    { "e34e94ec-e0d8-4200-afa2-7dfa29ffa70e", "1", "Admin", "Admin" },
+                    { "e8da42f8-3c92-41fb-95bc-7c2770ee4587", "2", "User", "User" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Historiks_Mjeket_MjekuId",
+                table: "Historiks",
+                column: "MjekuId",
+                principalTable: "Mjeket",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Historiks_Pacientet_PacientId",
+                table: "Historiks",
+                column: "PacientId",
+                principalTable: "Pacientet",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Historiks_Mjeket_MjekuId",
+                table: "Historiks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Historiks_Pacientet_PacientId",
+                table: "Historiks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Historiks",
+                table: "Historiks");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "16e9155a-f42f-4897-a913-b7f516792b61");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e34e94ec-e0d8-4200-afa2-7dfa29ffa70e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e8da42f8-3c92-41fb-95bc-7c2770ee4587");
+
+            migrationBuilder.RenameTable(
+                name: "Historiks",
+                newName: "Historiqet");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Historiks_PacientId",
+                table: "Historiqet",
+                newName: "IX_Historiqet_PacientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Historiks_MjekuId",
+                table: "Historiqet",
+                newName: "IX_Historiqet_MjekuId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Historiqet",
+                table: "Historiqet",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "464a115c-8dff-426b-a5f4-24530fbc8dcf", "1", "Admin", "Admin" },
+                    { "8aef10f4-ac67-41a2-8048-1f037c660774", "2", "User", "User" },
+                    { "b069e9ec-8bc0-4350-9041-3ba8fb4aef0b", "3", "Doctor", "Doctor" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Historiqet_Mjeket_MjekuId",
+                table: "Historiqet",
+                column: "MjekuId",
+                principalTable: "Mjeket",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Historiqet_Pacientet_PacientId",
+                table: "Historiqet",
+                column: "PacientId",
+                principalTable: "Pacientet",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
