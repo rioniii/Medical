3cbc46e8456a64ffed70c5ca447aab10535b95c9
ubejdaliu06 <ua68003@ubt-uni.net>
2024-10-21 02:40:41 +0200

    Migration Role

---
+++ ReactApp1.Server/Controllers/RoleController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactApp1.Server.Data;
+using ReactApp1.Server.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Role
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Role>>> GetRoles()
+        {
+            return await _context.Roles
+                .Include(r => r.Users)  // Include associated Users
+                .ToListAsync();
+        }
+
+        // GET: api/Role/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Role>> GetRole(int id)
+        {
+            var role = await _context.Roles
+                .Include(r => r.Users)  // Include associated Users
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return role;
+        }
+
+        // PUT: api/Role/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRole(int id, Role role)
+        {
+            if (id != role.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(role).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Role
+        [HttpPost]
+        public async Task<ActionResult<Role>> PostRole(Role role)
+        {
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRole", new { id = role.Id }, role);
+        }
+
+        // DELETE: api/Role/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            _context.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RoleExists(int id)
+        {
+            return _context.Roles.Any(e => e.Id == id);
+        }
+    }
+}
